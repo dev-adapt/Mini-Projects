20c4d3585f7a59c5c1483e3509272716c2525a28
Shruti Agnihtori <141560261+dev-adapt@users.noreply.github.com>
2024-04-05 20:00:01 +0530

    Add files via upload

---
+++ JAVASCRIPT/practice3/DOMprac.js
@@ -0,0 +1,8 @@
+let btn=document.createElement("button");
+btn.innerText="click me";
+btn.style.color="white";
+btn.style.backgroundColor="red";
+document.querySelector("body").prepend(btn);
+
+//question02
+let para=document.querySelector("p");
